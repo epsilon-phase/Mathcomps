8231b9a0cfd98eb5d44d58d32f30ef3698707392
Alexander White <jaked122@gmail.com>
2012-11-11 20:56:40 -0500

    LineChart Tested and Fixed
    The linechart is now reasonably stable. Unfortunately, the multiline
    component is no doubt thoroughly broken. I need to rewrite that before I
    can commit it.
    Also it supports some basic interpolation, so enjoyment is assured now.

--- src/bump/org/comp/ChartingThing.java
+++ src/bump/org/comp/ChartingThing.java
@@ -7,7 +7,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.FlatteningPathIterator;
 import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
 import java.awt.geom.Line2D.Float;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ public class ChartingThing extends Canvas {
 	public ChartingThing() {
 		super();
 		this.data = new ArrayList<Integer>();
+
 	}
 
 	public void setTicks(int x, int y) {
@@ -26,62 +29,90 @@ public class ChartingThing extends Canvas {
 	}
 
 	public void paint(Graphics g) {
-		Graphics2D put = (Graphics2D) g.create();
+		if (data.size() > 0) {
+			Graphics2D put = (Graphics2D) g.create();
 
-		float vstep;
-		float hstep = zoomfactor * (getWidth() / data.size());
-		vstep = zoomfactor * (getHeight() / (ChartingUtil.getdiff(data)) / 2);
-		// put the x-axis in the middle of the chart
-		put.translate(0, getHeight() / 2);
-		// Essentially configuring whether or not the antialiasing is on
-		RenderingHints h;
-		if (antialias) {
-			h = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-					RenderingHints.VALUE_ANTIALIAS_ON);
+			double vstep;
+			double hstep = (getWidth() / data.size());
 
-		} else {
-			h = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-					RenderingHints.VALUE_ANTIALIAS_OFF);
+			vstep = (getHeight() / (ChartingUtil.getdiff(data)));
+			// make sure it isn't too small.
+			if (vstep < .2) {
+				vstep = .2;
+			}
+
+			// Add rendering hints to the graphics objects according to the
+			// members of the class.
+			RenderingHints h;
+			if (antialias) {
+				h = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+				h.add(new RenderingHints(RenderingHints.KEY_INTERPOLATION,
+						RenderingHints.VALUE_INTERPOLATION_BICUBIC));
+			} else {
+				h = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_OFF);
+			}
+			put.addRenderingHints(h);
+			put.setColor(Color.black);
+			// Draw the horizontal Axis.
+			put.draw(new Line2D.Double(0, getHeight() / 2, getWidth(),
+					getHeight() / 2));
+			for (int i = 0; i * vstep < getWidth(); i++)
+				if (i % xtick == 0)
+					put.draw(new Line2D.Double(i * hstep, 0 + getHeight() / 2,
+							i * hstep, 5 + getHeight() / 2));
+			// draw the vertical ticks.
+			put.draw(new Line2D.Double(getWidth() / 2, -getHeight(),
+					getWidth() / 2, getHeight()));
+			for (int i = 0; (i * vstep) < (getHeight()); i++) {
+				// if the integer is a multiple of i, draw the tick.
+				if (i % ytick == 0) {
+					put.draw(new Line2D.Double(getWidth() / 2, i * vstep,
+							getWidth() / 2 + 5, i * vstep));
+				}
+			}
+			// put the x-axis in the middle of the chart
+			// put.translate(0, getHeight() / 2);
+			for (int i = 0; i < data.size(); i++) {
+
+				if (i % xtick == 0)// Draw the ticks on the x-axis
+					put.draw(new Line2D.Double(hstep * (i - 1), 0, hstep * (i),
+							5));
+			}
+			// preserve the ticks.
+			put.setXORMode(Color.gray);
+			// set the color to the background color member
+			put.setColor(backgroundcolor);
+			// Fill the background of the graph
+			put.fill(new Rectangle2D.Float(-getWidth() / 2, -getHeight() / 2,
+					getWidth() / 2, getHeight() / 2));
+			// allow the data line to overwrite the current colors.
+			put.setPaintMode();
+			put.setColor(linecolor);
+			Path2D.Double e = new Path2D.Double();
+			e.moveTo(0, vstep * data.get(0));
+			for (int i = 2; i < data.size(); i++) {
+				if (!curvelines)
+					e.lineTo(hstep * i, data.get(i) * vstep);
+				else {
+					// quadratic interpolation
+					e.quadTo(hstep * (i - 1) + (i % 3 == 0 ? 4 : 1), vstep
+							* data.get(i - 1) + 2, i * hstep, data.get(i)
+							* vstep);
+				}
+
+			}
+			put.draw(e);
 		}
-		put.addRenderingHints(h);
-		put.setColor(Color.black);
-
-		for (int i = 0; i < data.size(); i++) {
-			put.draw(new Line2D.Float(hstep * (i - 1), 0, hstep * (i), 0));
-			if (i % xtick == 0)
-				put.draw(new Line2D.Float(hstep * (i - 1), -vstep, hstep * (i),
-						vstep));
-		}// draw the vertical ticks.
-		put.draw(new Line2D.Float(0, -getHeight() / 2, 0, getHeight() / 2));
-		for (int i = 0; i * vstep < getHeight() / (2 * ChartingUtil.Max(data)); i++) {
-			// start in the middle and radiate outwards.
-			put.draw(new Line2D.Float(0, vstep * i, hstep, vstep * i));
-			put.draw(new Line2D.Float(0, -vstep * i, hstep, -vstep * i));
-		}
-		// preserve the ticks.
-		put.setXORMode(Color.gray);
-		put.draw(new Rectangle2D.Float(-getWidth() / 2, -getHeight() / 2,
-				getWidth() / 2, getHeight() / 2));
-		// allow the data line to overwrite the current colors.
-		put.setPaintMode();
-		put.setColor(linecolor);
-		put.setStroke(new BasicStroke(zoomfactor));
-		for (int i = 1; i < data.size(); i++) {
-			put.draw(new Line2D.Float(hstep * (i - 1), data.get(i - 1) * vstep,
-					i * hstep, data.get(i) * vstep));
-		}
-
 	}
 
+	private boolean curvelines = true;
 	private Color backgroundcolor = Color.white;
 	/**
 	 * The value that determines whether or not the lines should be antialiased.
 	 */
 	private boolean antialias = true;
-	/**
-	 * The amount to zoom in by
-	 */
-	private float zoomfactor = 1.0f;
 
 	public void SetColor(Color g) {
 		linecolor = g;
@@ -91,12 +122,20 @@ public class ChartingThing extends Canvas {
 		return linecolor;
 	}
 
-	public Color linecolor;
+	public Color linecolor = Color.blue;
 
 	public void addData(int[] data) {
 		this.data.ensureCapacity(this.data.size() + data.length);
 		for (int i : data)
+			// Flip the points to fit with Java's odd coordinate scheme
 			this.data.add(i);
+		repaint();
+	}
+
+	public void addDataPoint(int d) {
+		// Flip the points to fit with Java's odd coordinate scheme.
+		this.data.add(d);
+		repaint();
 	}
 
 	public void setData(int[] data) {
@@ -105,6 +144,7 @@ public class ChartingThing extends Canvas {
 		this.data.ensureCapacity(data.length);
 		for (int i : data)
 			this.data.add(i);
+		repaint();
 	}
 
 	/**
@@ -120,21 +160,6 @@ public class ChartingThing extends Canvas {
 	 */
 	public void setAntialias(boolean antialias) {
 		this.antialias = antialias;
-	}
-
-	/**
-	 * @return the zoomfactor
-	 */
-	public float getZoomfactor() {
-		return zoomfactor;
-	}
-
-	/**
-	 * @param zoomfactor
-	 *            the zoomfactor to set
-	 */
-	public void setZoomfactor(float zoomfactor) {
-		this.zoomfactor = zoomfactor;
 	}
 
 	int ytick = 5, xtick = 5;
--- src/bump/org/test/ChartingThingTest.java
+++ src/bump/org/test/ChartingThingTest.java
@@ -2,14 +2,23 @@ package bump.org.test;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import bump.org.comp.ChartingThing;
+import javax.swing.JTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class ChartingThingTest extends JFrame {
 
 	private JPanel contentPane;
+	private ChartingThing qw;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -37,6 +46,54 @@ public class ChartingThingTest extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
 
+		qw = new ChartingThing();
+		qw.setData(new int[] { 23, 34, 52, 23, 35, 12 });
+		qw.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				qw.addDataPoint(e.getY());
+			}
+		});
+		contentPane.add(qw, BorderLayout.CENTER);
+
+		textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				if (e.getKeyCode() == e.VK_ENTER) {
+					qw.addDataPoint(Integer.parseInt(textField.getText()));
+					textField.setText("");
+				}
+			}
+		});
+		contentPane.add(textField, BorderLayout.SOUTH);
+		textField.setColumns(10);
+	}
 }
